6b71c5b47952e21f637a5e6a421bdf94f84ae803
Araya515 <tomoya5.15@me.com>
2022-03-13 15:58:59 +0900

    ClientForm

---
+++ ClientForm/ClientForm/ClientForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ClientForm
+{
+    partial class clientform
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClientTextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ClientTextBox
+            // 
+            this.ClientTextBox.BackColor = System.Drawing.Color.Black;
+            this.ClientTextBox.ForeColor = System.Drawing.Color.White;
+            this.ClientTextBox.Location = new System.Drawing.Point(12, 79);
+            this.ClientTextBox.Multiline = true;
+            this.ClientTextBox.Name = "ClientTextBox";
+            this.ClientTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ClientTextBox.Size = new System.Drawing.Size(471, 170);
+            this.ClientTextBox.TabIndex = 0;
+            this.ClientTextBox.WordWrap = false;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(409, 255);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "送信";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(438, 19);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "http://localhost:8080/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "HTMLデータ取得";
+            // 
+            // clientform
+            // 
+            this.AcceptButton = this.SendButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.ClientTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "clientform";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ClientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ClientTextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ClientForm/ClientForm/ClientForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientForm
+{
+    public partial class clientform : Form
+    {
+        public clientform()
+        {
+            InitializeComponent();
+        }
+
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        readonly HttpClient client = new HttpClient();
+
+        private async void SendButton_Click(object sender, EventArgs e)
+        {
+            SendButton.Enabled = false;
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(textBox1.Text);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+
+                ClientTextBox.Text = responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", ex.Message);
+            }
+            SendButton.Enabled = true;
+        }
+    }
+}
